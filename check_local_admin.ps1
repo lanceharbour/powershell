8db4b851d9423e16a4cf758c4cff1230c99ea37b
lharbour <llharbour@yahoo.com>
2019-12-18 15:34:21 -0700

    added additional powershell files

---
+++ check_local_admin.ps1
@@ -0,0 +1,36 @@
+﻿###############################################################################
+# Checks all servers for local account renamed local admin accounts
+#
+# lance.harbour
+###############################################################################
+$emailbody = "Local account is missing on the following servers:<br><br>"
+$fcount = 0
+$sender = "email address"
+$recipients = "email address"
+
+$user1 = ""
+$user2 = ""
+$servers = Get-ADComputer -Filter {(OperatingSystem -like "Windows Server*") -and (enabled -eq "true") -and (name -notlike "*godc*")} | Select-Object name | Sort-Object name
+foreach ($server in $servers)
+    {
+    if (Test-Connection $server.name -Count 2 -ea 0 -Quiet)
+        {
+        $usearch = Get-WmiObject -ComputerName $server.name win32_useraccount | where {($_.name -eq $user1) -or ($_.name -eq $user2)}
+        if ($usearch.Name -notlike $user1)
+            {
+            $fcount = $fcount + 1
+            Write-Host $server.name " $user1 missing"   
+            #$emailbody = "$emailbody"+$server.name+"<br>"
+            }
+        if ($usearch.Name -notlike $user2)
+            {
+            $fcount = $fcount + 1
+            Write-Host $server.Name " $user2 missing"
+            }
+        }
+    }
+
+if ($fcount -gt 0)
+    {  
+    Send-MailMessage -From $sender -To $recipients -Subject "*** Local Account Missing ***" -Body $emailbody -BodyAsHtml -Priority High -SmtpServer mail
+    }
---
+++ installed_sw_check.ps1
@@ -0,0 +1,45 @@
+﻿##################################################################
+#Searches AD for specified computers then checks to see if       #
+#specified system is online then checks if software is installed.#
+##################################################################
+$logfile = "C:\scripts\sw_check.txt" 
+$searchOU = ""
+$sw = "softare to search for"
+$servers = Get-ADComputer -Filter {((OperatingSystem -like "Windows 10*") -or (OperatingSystem -like "Windows 7*")) -and (enabled -eq "true")} -SearchBase "$searchOU" | Select-Object name | Sort-Object name
+$oservers = @()
+
+Out-File -FilePath $logfile -Force
+foreach ($server in $servers)
+    {
+        if (Test-Connection $server.name -Count 1 -ea 0 -Quiet)
+        {
+        $oservers = $oservers + $server.name
+        } 
+        else
+        {
+        $server.name+","+"unreachable" | Out-File -FilePath $logfile -Append       
+        }  
+    }
+
+foreach ($oserver in $oservers)
+    {
+    $oserver
+    $sw_ver = Get-WmiObject -ComputerName $oserver -Class win32_product -ErrorAction Continue -ErrorVariable noresult | 
+    Select-Object Name, Version | Where-Object {$_.name -like "*$sw*"} 
+
+    if ($noresult)
+        {
+        $oserver+","+"Run Error" | Out-File -FilePath $logfile -Append
+        }
+    else
+        {
+        if ($sw_ver.name -like "*$sw*")
+            {
+            $oserver+","+$sw_ver.name+","+$sw_ver.version | Out-File -FilePath $logfile -Append    
+            }
+        else
+            {
+            $oserver+","+"not installed" | Out-File -FilePath $logfile -Append 
+            }
+        }
+    }
