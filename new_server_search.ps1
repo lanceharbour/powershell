d3d6f916f452e6b67bab5b7446a107ea8da137f0
lharbour <llharbour@yahoo.com>
2021-01-20 10:45:47 -0700

    updated folder structure

--- new_server_search.ps1
+++
@@ -1,8 +0,0 @@
-﻿#################################################################################
-#Searches AD for servers created in the last 30 days.                           #
-#################################################################################
-$newservers = Get-ADComputer -Filter {(OperatingSystem -like "Windows Server*")} -Properties name,enabled,whencreated | Where-Object {$_.whencreated -ge (get-date).adddays(-31)} 
-foreach ($newserver in $newservers)
-    {
-    Write-Host $newserver.name $newserver.whencreated
-    }
